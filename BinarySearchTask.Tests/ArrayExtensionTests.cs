9b7310de129c7654cbdd48643d08e95fac556980
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-20 16:15:23 +0000

    Update ArrayExtensionTests.cs

--- BinarySearchTask.Tests/ArrayExtensionTests.cs
+++ BinarySearchTask.Tests/ArrayExtensionTests.cs
@@ -12,7 +12,7 @@ namespace BinarySearchTask.Tests
         [TestCase(new[] {1, 3, 4, 6, 8, 9, 11}, 11, ExpectedResult = 6)]
         [TestCase(new[] {1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634}, 144, ExpectedResult = 9)]
         [TestCase(new[] {1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377}, 21, ExpectedResult = 5)]
-        public int BinarySearch_Return_Index_Of_Value_In_Array(int[] source, int value)
+        public int BinarySearch_ReturnIndexOfValueInArray(int[] source, int value)
         {
             return BinarySearch(source, value).Value;
         }
@@ -23,14 +23,14 @@ namespace BinarySearchTask.Tests
         [TestCase(new[] {1, 3, 4, 6, 8, 9, 11}, 11, ExpectedResult = 6)]
         [TestCase(new int[] { }, 144, ExpectedResult = null)]
         [TestCase(new[] {1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377}, 21, ExpectedResult = 5)]
-        public int? BinarySearch_Return_Null(int[] source, int value)
+        public int? BinarySearch_ReturnNull(int[] source, int value)
         {
             return BinarySearch(source, value);
         }
 
         [Test]
-        public static void BinarySearch_Throw_ArgumentNullException_If_Source_Array_Is_Null()
-            => Assert.Throws<ArgumentOutOfRangeException>(() =>
+        public static void BinarySearch_SourceArrayIsNull_ThrowArgumentNullException()
+            => Assert.Throws<ArgumentNullException>(() =>
                 BinarySearch(null, 1), $"For null source must be throw ArgumentNullException.");
     }
 }
